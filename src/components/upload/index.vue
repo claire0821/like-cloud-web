fa1ff6e4656fc3cb4b6f1ed21782b89260bea17e
wujingling <claire0821@126.com>
2023-04-18 11:32:26 +0800

    save

--- src/components/upload/index.vue
+++ src/components/upload/index.vue
@@ -81,7 +81,7 @@ export default defineComponent({
         const uploadRefs = shallowRef<InstanceType<typeof ElUpload>>()
         const action = ref(`${config.baseUrl}${config.urlPrefix}/common/upload/${props.type}`)
         const headers = computed(() => ({
-            token: userStore.token,
+            token: 'Like ' + userStore.token,
             version: config.version
         }))
         const visible = ref(false)
@@ -121,7 +121,7 @@ export default defineComponent({
         const getAccept = computed(() => {
             switch (props.type) {
                 case 'image':
-                    return '.jpj,.png,.gif,.jpeg,.ico,.bmp'
+                    return '.jpj,.jpg,.png,.gif,.jpeg,.ico,.bmp'
                 case 'video':
                     return '.wmv,.avi,.mov,.mp4,.flv,.rmvb'
                 default:
